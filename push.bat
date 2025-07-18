481c82e36598362ab9e615b3e11968d28bdc6100
RubyChengLab <comet.hope.1227@gmail.com>
2025-07-18 09:19:48 +0800

    Automatic updates 2025/07/18 週五  9:19:48.86

--- push.bat
+++ push.bat
@@ -1,10 +1,10 @@
 @echo off
-echo 正在將網站推送到 GitHub...
+echo 正在將網站推送到 GitHub... (autonew)
 
 cd /d "C:\Users\Ruby\myblog"
 
 git add .
-git commit -m "自動更新網站 %date% %time%"
+git commit -m "Automatic updates %date% %time%"
 git push origin master
 
 pause
