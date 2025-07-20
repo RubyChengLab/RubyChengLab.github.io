c63bb108d4239ddb7432ccb66aea2c6ceab4a0e6
RubyChengLab <comet.hope.1227@gmail.com>
2025-07-20 12:50:38 +0800

    Automatic updates 2025/07/20 週日 12:50:38.28

--- assets/js/popular.js
+++ assets/js/popular.js
@@ -2,9 +2,14 @@ import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.23.
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  // 其他設定參數...
+  apiKey: "AIzaSyBmR7K4ECZA0Vv0PlHn6dMxg5P06UsBnq0",
+  authDomain: "falling-comet-lab-blog.firebaseapp.com",
+  databaseURL: "https://falling-comet-lab-blog-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "falling-comet-lab-blog",
+  storageBucket: "falling-comet-lab-blog.firebasestorage.app",
+  messagingSenderId: "275403715950",
+  appId: "1:275403715950:web:25383ec082d6ff3338bd7f",
+  measurementId: "G-TRDSPMNYQK"
 };
 
 if (!getApps().length) {
